05f11a2c2534e9160b0de1489f0281feb68f7f6d
AlexandraUgryumova <124123433+AlexandraUgryumova@users.noreply.github.com>
2023-03-24 10:06:43 +0500

    itial commit

---
+++ Угрюмова практика 14 Фильмотека/Filmoteka.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Угрюмова_практика_14_Фильмотека
+{
+    internal class Filmoteka
+    {
+        string name;
+        string production;
+        TimeSpan time;
+        string genre;
+        public void Film(string name, string production, TimeSpan time, string genre)
+        {
+            this.name = name;
+            this.production = production;
+            this.time = time;
+            this.genre = genre;
+        }
+        class TVshow : Filmoteka
+        {
+            int count;
+        }
+        class FeatureFilm : Filmoteka
+        {
+            DateTime yearOfIssue;
+            bool color;
+        }
+    }
+    abstract class KKK
+    {
+
+    }
+}
---
+++ Угрюмова практика 14 Фильмотека/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Угрюмова_практика_14_Фильмотека
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Filmoteka fm = new Filmoteka();
+            fm.Film("Секс в большом городе", "HBO", new TimeSpan(1, 20, 10), "комедия");
+        }
+    }
+}
